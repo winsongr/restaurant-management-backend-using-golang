ca04a90fb72ac4dc9dbd4125c87126b039b255bb
Winson G <96694861+winsongr@users.noreply.github.com>
2022-02-16 21:56:12 +0530

    func

--- controllers/menucontroller.go
+++ controllers/menucontroller.go
@@ -1,25 +1,46 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
 
-func GetMenus() gin.HandlerFunc{
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while listing menu"})
+		}
+		var allMenus []bson.M
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
+	}
+}
+
+func GetMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 	}
 }
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-func GetMenu() gin.HandlerFunc{
-	return func(c *gin.Context) {
-		
 	}
 }
-func CreateMenu() gin.HandlerFunc{
+func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
-	}
-}
-func UpdateMenu() gin.HandlerFunc{
-	return func(c *gin.Context) {
-		
+
 	}
 }
--- models/ordermodel.go
+++ models/ordermodel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Order struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Order_Date time.Time          `json:"order_date" validate:"required"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Table_id   *string            `json:"table_id"`
+	Order_id   string             `json:"order_id"  validate:"required"`
+}
