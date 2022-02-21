71e3e1c9a9cb759bd8b99920421bfe4c976b6c22
Winson G <96694861+winsongr@users.noreply.github.com>
2022-02-21 19:39:13 +0530

    Update databaseconnection.go

--- database/databaseconnection.go
+++ database/databaseconnection.go
@@ -1,20 +1,20 @@
 package database
 
-import(
+import (
 	"context"
 	"fmt"
-	"log"
-	"time"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
 )
 
-func DBinstance() *mongo.Client{
+func DBinstance() *mongo.Client {
 	MongoDb := "mongodb://localhost:27017"
 	fmt.Print(MongoDb)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -22,16 +22,16 @@ func DBinstance() *mongo.Client{
 
 	err = client.Connect(ctx)
 
-	if err ! = nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printlm("connected to mongodb")
+	fmt.Println("connected to mongodb")
 	return client
 }
 
 var Client *mongo.Client = DBinstance()
 
-func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection  {
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
 
 	return collection
