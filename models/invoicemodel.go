1ae8fdc9325d2a54f9242d7c054f07ccc734147a
Winson G <96694861+winsongr@users.noreply.github.com>
2022-02-21 20:47:43 +0530

    Update invoicemodel.go

--- models/invoicemodel.go
+++ models/invoicemodel.go
@@ -7,7 +7,7 @@ import (
 
 type Invoice struct {
 	ID               primitive.ObjectID `bson:"_id"`
-	Invoice_id       string				`json:"invoice_id`
+	Invoice_id       string				`json:"invoice_id"`
 	Order_id         string				`json:"order_id"`
 	Payment_method   *string			`json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
 	Payment_status   *string			`json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
