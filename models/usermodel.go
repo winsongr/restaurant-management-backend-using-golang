b78f04fe56debb5c91079a3ccf1bab98cc9a6f00
Winson G <96694861+winsongr@users.noreply.github.com>
2022-02-21 20:46:21 +0530

    Update usermodel.go

--- models/usermodel.go
+++ models/usermodel.go
@@ -12,7 +12,7 @@ type User struct {
 	Last_name     *string            `json:"last_name"  validate:"required,min=2,max=100"`
 	Password      *string            `json:"password"  validate:"required,min=6"`
 	Email         *string            `json:"email"  validate:"email,required"`
-	Avatar        *string            `json:"avatar"  "`
+	Avatar        *string            `json:"avatar"  `
 	Phone         *string            `json:"phone"  validate:"required"`
 	Token         *string            `json:"token"`
 	Refresh_Token *string            `json:"refresh_token"`
